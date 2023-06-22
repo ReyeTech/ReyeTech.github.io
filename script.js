cc182b3dc6eb67849913d53342138dfd6d4e9678
Swapnil Bhat <swapnilbhat.iitb@gmail.com>
2023-06-22 23:38:59 +0530

    feat: startup-template from free css

--- script.js
+++
@@ -1,8 +0,0 @@
-// script.js
-document.querySelector('form').addEventListener('submit', function (e) {
-    e.preventDefault(); // Prevent form submission
-    // You can add your own logic here, such as sending the form data to your server or displaying a success message.
-    alert('Thank you for your message!');
-    this.reset(); // Reset the form
-  });
-  
